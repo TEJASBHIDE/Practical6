8d499b634d5bdcb13a80cf2b36bf6822f98708ec
TEJASBHIDE <141414238+TEJASBHIDE@users.noreply.github.com>
2025-04-16 23:10:31 +0530

    Create index.js

---
+++ Q5/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.end('🎉 Welcome to the NodeJS app deployed by Ansible Pull!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
